08994fa33e1b33e871c64ce8ded0342029c9a968
Mqbretrofit <matrai.attila975@gmail.com>
2026-09-01 15:23:21 +0200

    Expose exact per-model APK parameter definitions

--- custom_components/tmt_chow/diagnostics.py
+++ custom_components/tmt_chow/diagnostics.py
@@ -10,6 +10,7 @@ from homeassistant.helpers.redact import async_redact_data
 
 from .const import CONF_CERTIFICATE_ARN, CONF_CERTIFICATE_PEM, CONF_PRIVATE_KEY, DOMAIN
 from .hub import TmtChowHub
+from .model_parameter_schemas import parameter_name, parameter_options
 
 _REDACT = {CONF_CERTIFICATE_PEM, CONF_PRIVATE_KEY, CONF_CERTIFICATE_ARN}
 
@@ -33,6 +34,40 @@ async def async_get_config_entry_diagnostics(
             "controller_family": hub.controller_family,
             "controller_capabilities": sorted(hub.controller_capabilities),
             "parameter_schema_verified": hub.parameter_schema_verified,
+            "model_parameter_schema_count": (
+                len(hub.model_parameter_schema)
+                if hub.model_parameter_schema is not None
+                else 0
+            ),
+            "model_parameter_schema": (
+                [
+                    {
+                        "index": index,
+                        "key": spec[1],
+                        "name": parameter_name(spec),
+                        "kind": spec[0],
+                        "option_key": spec[2],
+                        "options": list(parameter_options(spec)),
+                        "parameter_type": spec[3],
+                        "level": spec[4],
+                        "default": spec[5],
+                        "offset": spec[6],
+                        "max_value_hint": spec[7],
+                        "bit_index": spec[8],
+                        "big_endian": spec[9],
+                        "bit_count": spec[10],
+                        "minimum": spec[11],
+                        "maximum": spec[12],
+                        "increment": spec[13],
+                        "multiple": spec[14],
+                        "unit_key": spec[15],
+                        "off_value": spec[16],
+                    }
+                    for index, spec in enumerate(
+                        hub.model_parameter_schema or (), start=1
+                    )
+                ]
+            ),
             "parameters": hub.parameters,
             "attributes": hub.attributes,
         },
