b76dc26c6221f4351ed47716358d853046d21a82
Mqbretrofit <matrai.attila975@gmail.com>
2026-09-20 12:24:45 +0200

    Load ARM64 helper module in OURANOS probe tests

--- tests/test_ouranos_ha_probe.py
+++ tests/test_ouranos_ha_probe.py
@@ -44,10 +44,27 @@ def _load_probe():
         sys.modules.setdefault("homeassistant.core", core)
         sys.modules.setdefault("homeassistant.helpers", helpers)
         sys.modules.setdefault("homeassistant.helpers.aiohttp_client", aiohttp_client)
-    spec = importlib.util.spec_from_file_location("tmt_chow_ouranos_ha_probe", PROBE_PATH)
+    package_name = "tmt_chow_probe_testpkg"
+    package = types.ModuleType(package_name)
+    package.__path__ = [str(PROBE_PATH.parent)]
+    sys.modules.setdefault(package_name, package)
+
+    arm64_name = f"{package_name}.ouranos_arm64"
+    if arm64_name not in sys.modules:
+        arm64_path = PROBE_PATH.with_name("ouranos_arm64.py")
+        arm64_spec = importlib.util.spec_from_file_location(arm64_name, arm64_path)
+        assert arm64_spec is not None
+        assert arm64_spec.loader is not None
+        arm64_module = importlib.util.module_from_spec(arm64_spec)
+        sys.modules[arm64_name] = arm64_module
+        arm64_spec.loader.exec_module(arm64_module)
+
+    probe_name = f"{package_name}.ouranos_ha_probe"
+    spec = importlib.util.spec_from_file_location(probe_name, PROBE_PATH)
     assert spec is not None
     assert spec.loader is not None
     module = importlib.util.module_from_spec(spec)
+    sys.modules[probe_name] = module
     spec.loader.exec_module(module)
     return module
 
